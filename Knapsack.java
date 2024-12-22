afc6b883bc5e4ca91a5687db07df31489a515223
Pratham Potdar <prthampotdar797@gmail.com.com>
2024-12-22 20:38:38 +0530

    change Files

--- Knapsack.java
+++ Knapsack.java
@@ -5,14 +5,14 @@ public class Knapsack {
    public Knapsack() {
    }
 
-   static int knapsack(int var0, int[] var1, int[] var2, int var3) {
+   static int knapsack(int var0, int[] sc, int[] var2, int var3) {
       int[][] var4 = new int[var3 + 1][var0 + 1];
 
       for(int var5 = 0; var5 <= var3; ++var5) {
          for(int var6 = 0; var6 <= var0; ++var6) {
             if (var5 != 0 && var6 != 0) {
-               if (var1[var5 - 1] <= var6) {
-                  var4[var5][var6] = Math.max(var2[var5 - 1] + var4[var5 - 1][var6 - var1[var5 - 1]], var4[var5 - 1][var6]);
+               if (sc[var5 - 1] <= var6) {
+                  var4[var5][var6] = Math.max(var2[var5 - 1] + var4[var5 - 1][var6 - sc[var5 - 1]], var4[var5 - 1][var6]);
                } else {
                   var4[var5][var6] = var4[var5 - 1][var6];
                }
@@ -25,29 +25,29 @@ public class Knapsack {
       return var4[var3][var0];
    }
 
-   public static void main(String[] var0) {
-      Scanner var1 = new Scanner(System.in);
-      System.out.print("Enter the number of items: ");
-      int var2 = var1.nextInt();
-      int[] var3 = new int[var2];
-      int[] var4 = new int[var2];
-      System.out.println("Enter the values of the items: ");
+   public static void main(String[] args) {
+      try (Scanner sc = new Scanner(System.in)) {
+         System.out.print("Enter the number of items: ");
+         int var2 = sc.nextInt();
+         int[] var3 = new int[var2];
+         int[] var4 = new int[var2];
+         System.out.println("Enter the values of the items: ");
 
-      int var5;
-      for(var5 = 0; var5 < var2; ++var5) {
-         var3[var5] = var1.nextInt();
+         int var5;
+         for(var5 = 0; var5 < var2; ++var5) {
+            var3[var5] = sc.nextInt();
+         }
+
+         System.out.println("Enter the weights of the items: ");
+
+         for(var5 = 0; var5 < var2; ++var5) {
+            var4[var5] = sc.nextInt();
+         }
+
+         System.out.print("Enter the capacity of the knapsack: ");
+         var5 = sc.nextInt();
+         int var6 = knapsack(var5, var4, var3, var2);
+         System.out.println("The maximum profit is: " + var6);
       }
-
-      System.out.println("Enter the weights of the items: ");
-
-      for(var5 = 0; var5 < var2; ++var5) {
-         var4[var5] = var1.nextInt();
-      }
-
-      System.out.print("Enter the capacity of the knapsack: ");
-      var5 = var1.nextInt();
-      int var6 = knapsack(var5, var4, var3, var2);
-      System.out.println("The maximum profit is: " + var6);
-      var1.close();
    }
 }
--- numberRhombus.java
+++ numberRhombus.java
@@ -1,41 +1,41 @@
 import java.util.Scanner;
+
 class numberRhombus {
-  public static void main(String args[]) 
-    {
-        Scanner in = new Scanner(System.in);
-        //System.out.print("Input the number:  ");
-        int n = in.nextInt();
-// program to display the number rhombus structure
-for (int i=1; i<=n; i++ ) {
-  for (int j=n; j>=1; j-- ) {
-    if(j>i) 
-      System.out.print(" ");
-    else 
-      System.out.print(j);
+  public static void main(String args[]) {
+    Scanner in = new Scanner(System.in);
+    System.out.print("Input the number: ");
+    int n = in.nextInt();
+    // program to display the number rhombus structure
+    for (int i = 1; i <= n; i++) {
+      for (int j = n; j >= 1; j--) {
+        if (j > i)
+          System.out.print(" ");
+        else
+          System.out.print(j);
+      }
+      for (int j = 2; j <= n; j++) {
+        if (j > i)
+          System.out.print("");
+        else
+          System.out.print(j);
+      }
+      System.out.print("\n");
+    }
+    for (int i = n - 1; i >= 1; i--) {
+      for (int j = n; j >= 1; j--) {
+        if (j > i)
+          System.out.print(" ");
+        else
+          System.out.print(j);
+      }
+      for (int j = 2; j <= n; j++) {
+        if (j > i)
+          System.out.print("");
+        else
+          System.out.print(j);
+      }
+      System.out.print("\n");
+    }
+    in.close();
   }
-  for(int j=2; j<=n; j++){
-    if(j>i)
-      System.out.print("");
-    else 
-      System.out.print(j);
-  }
-  System.out.print("\n");
 }
-for (int i=n-1; i>=1; i-- ) {
-  for (int j=n; j>=1; j-- ) {
-    if(j>i) 
-      System.out.print(" ");
-    else 
-      System.out.print(j);
-  }
-  for(int j=2; j<=n; j++){
-    if(j>i)
-      System.out.print("");
-    else 
-      System.out.print(j);
-  }
-   System.out.print("\n");
-}
-}
-}
-  
