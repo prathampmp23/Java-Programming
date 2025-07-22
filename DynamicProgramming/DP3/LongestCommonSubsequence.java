de54f9f0bd4efcb6ed5cdef7cb017eb631ba8a76
Pratham Potdar <prathampotdar797@gmail.com>
2025-07-22 17:14:59 +0530

    new program approach added in DP3

--- DynamicProgramming/DP3/LongestCommonSubsequence.java
+++ DynamicProgramming/DP3/LongestCommonSubsequence.java
@@ -3,23 +3,82 @@ package DynamicProgramming.DP3;
 public class LongestCommonSubsequence {
 
     // using recursion
-    public static int lcs(String str1, String str2, int n, int m) {
+    public static int lcsRec(String str1, String str2, int n, int m) {
         if (n == 0 || m == 0) {
             return 0;
         }
         if (str1.charAt(n - 1) == str2.charAt(m - 1)) { // same
-            return lcs(str1, str2, n - 1, m - 1) + 1;
+            return lcsRec(str1, str2, n - 1, m - 1) + 1;
         } else { // diff
-            int ans1 = lcs(str1, str2, n - 1, m);
-            int ans2 = lcs(str1, str2, n, m - 1);
+            int ans1 = lcsRec(str1, str2, n - 1, m);
+            int ans2 = lcsRec(str1, str2, n, m - 1);
             return Math.max(ans1, ans2);
         }
+    }
+
+    // using memoization O(n*m)
+    public static int lcsMemo(String str1, String str2, int n, int m, int dp[][]) {
+        if (n == 0 || m == 0) {
+            return 0;
+        }
+        // already calculated
+        if (dp[n][m] != -1) {
+            return dp[n][m];
+        }
+        if (str1.charAt(n - 1) == str2.charAt(m - 1)) { // same
+            return dp[n][m] = lcsMemo(str1, str2, n - 1, m - 1, dp) + 1;
+        } else { // diff
+            int ans1 = lcsMemo(str1, str2, n - 1, m, dp);
+            int ans2 = lcsMemo(str1, str2, n, m - 1, dp);
+            return dp[n][m] = Math.max(ans1, ans2);
+        }
+    }
+
+    // using tabulation O(n*m)
+    public static int lcsTabu(String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+        int dp[][] = new int[n+1][m+1];
+        // initialization 0th row and column
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < m + 1; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) { // same
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else { // diff
+                    int ans1 = dp[i - 1][j];
+                    int ans2 = dp[i][j - 1];
+                    dp[i][j] = Math.max(ans1, ans2);
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 
     public static void main(String[] args) {
         String str1 = "abcde";
         String str2 = "ace";
-        System.out.println("LCS length = "+lcs(str1, str2, str1.length(), str2.length()));
+        System.out.println("LCS length using recursion = " + lcsRec(str1, str2, str1.length(), str2.length()));
 
+        int dp[][] = new int[str1.length() + 1][str2.length() + 1];
+        int n = str1.length();
+        int m = str2.length();
+        // initialization
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < m + 1; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        System.out.println("LCS length using memoization = " + lcsMemo(str1, str2, n, m, dp));
+
+        System.out.println("LCS length using tabulation = " + lcsTabu(str1, str2));
     }
 }
