a4afe05f8190b53f65badb0a60d4bc195f89e013
Pratham Potdar <prathampotdar797@gmail.com>
2025-07-24 15:23:46 +0530

    new program added in Dp5

--- DynamicProgramming/DP5/MountainRanges.java
+++ DynamicProgramming/DP5/MountainRanges.java
@@ -1,5 +1,27 @@
 package DynamicProgramming.DP5;
 
 public class MountainRanges {
-    
+    public static int mountainRanges(int n) {
+        int dp[] = new int[n + 1];
+
+        // initialization
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                // Ci = cj * ci-j-1
+                int inside = dp[j];
+                int outside = dp[i - j - 1];
+                dp[i] += inside * outside;
+            }
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println("Mountain Ranges = " + mountainRanges(n));
+
+    }
 }
