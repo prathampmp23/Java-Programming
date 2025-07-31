3e0b140da07cfadc829589218e7a965bd4b4b2c2
Pratham Potdar <prathampotdar797@gmail.com>
2025-07-31 23:35:22 +0530

    New code added

--- Arrays1D/LCSinArray.java
+++ Arrays1D/LCSinArray.java
@@ -32,36 +32,30 @@ public class LCSinArray {
 
     // optimal
     public static int lcs2(int nums[]) {
-        if (nums.length == 0)
-            return 0;
-
+        if (nums.length == 0) return 0;
         HashSet<Integer> set = new HashSet<>();
         for (int num : nums) {
             set.add(num);
         }
-
         int longest = 0;
-
         for (int num : nums) {
             // Check if 'num' is the start of a sequence
             if (!set.contains(num - 1)) {
                 int currentNum = num;
                 int count = 1;
-
                 while (set.contains(currentNum + 1)) {
                     currentNum++;
                     count++;
                 }
-
                 longest = Math.max(longest, count);
             }
         }
-
         return longest;
     }
 
     public static void main(String[] args) {
         int arr[] = { 100, 4, 200, 1, 3, 2 };
+        System.out.println("Longest Consecutive sequence =  ");
         System.out.println(lcs(arr));
         System.out.println(lcs2(arr));
     }
---
+++ Arrays1D/Rotate90Degrees.java
@@ -0,0 +1,52 @@
+package Arrays1D;
+
+public class Rotate90Degrees {
+
+    public static void rotate(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                swap(matrix, i, j);
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            reverse(matrix, i, matrix[0].length - 1);
+        }
+    }
+
+    public static void swap(int arr[][], int i, int j) {
+        int temp = arr[i][j];
+        arr[i][j] = arr[j][i];
+        arr[j][i] = temp;
+    }
+
+    public static void reverse(int arr[][], int i, int j) {
+        int idx = 0;
+        while (idx < j) {
+            int temp = arr[i][j];
+            arr[i][j] = arr[i][idx];
+            arr[i][idx] = temp;
+            idx++;
+            j--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Rotated Matrix 90 degree = ");
+        rotate(matrix);
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
