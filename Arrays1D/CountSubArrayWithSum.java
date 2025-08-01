cc41f1e6a6a9de28168528de75909573776ce656
Pratham Potdar <prathampotdar797@gmail.com>
2025-08-01 23:53:08 +0530

    new code is added of subarray

--- Arrays1D/CountSubArrayWithSum.java
+++ Arrays1D/CountSubArrayWithSum.java
@@ -1,7 +1,32 @@
 package Arrays1D;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CountSubArrayWithSum {
+
+    public static int subarraySum(int[] nums, int k) {
+        int count = 0;
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1);
+
+        for (int num : nums) {
+            sum += num;
+
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+
     public static void main(String[] args) {
-        
+        int arr[] = { 1, 2, 3, -3, 11, 1, 4, 2, -3 };
+        System.out.println(subarraySum(arr, 3));
     }
 }
