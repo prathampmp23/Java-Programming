9ce3fb69708abb7b3ed1a249f035ba60712df191
Pratham Potdar <prathampotdar797@gmail.com>
2025-08-01 23:25:41 +0530

    added count program to 1D array

---
+++ Arrays1D/CountSubArrayWithSum.java
@@ -0,0 +1,7 @@
+package Arrays1D;
+
+public class CountSubArrayWithSum {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Arrays1D/SetMatrixZero.java
@@ -0,0 +1,90 @@
+package Arrays1D;
+
+public class SetMatrixZero {
+    // Brute - O(n*m) * O(n*m)
+    public static void setZeroes(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        boolean[] row = new boolean[n];
+        boolean[] col = new boolean[m];
+
+        // Step 1: mark rows and cols to be zeroed
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+
+        // Step 2: set zeroes
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    // optimize - O(2*n*m) (in-place)
+    public static void SetMatrixZero2(int matrix[][]) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int col0 = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    // mark the ith row
+                    matrix[i][0] = 0;
+                    // mark the jth col
+                    if (j != 0)
+                        matrix[0][j] = 0;
+                    else
+                        col0 = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][j] != 0) {
+                    // check for col & row
+                    if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+        // set first col 0
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        // set first row 0
+        if (col0 == 0) {
+            for (int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = { { 1, 1, 1 }, { 1, 0, 1 }, { 1, 1, 1 } };
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        setZeroes(matrix);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
