aa1a6244ed2bbb4f5b618d0dc2649216b88e89f6
Pratham Potdar <prathampotdar797@gmail.com>
2025-08-02 23:41:46 +0530

    majority element code added in arrays

--- Arrays1D/MajorityElement.java
+++ Arrays1D/MajorityElement.java
@@ -1,6 +1,10 @@
 package Arrays1D;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MajorityElement {
 
@@ -44,17 +48,38 @@ public class MajorityElement {
             if (count == 0) {
                 count = 1;
                 el = nums[i];
-            } 
-            else if (nums[i] == el) count++;
-            else count--;
+            } else if (nums[i] == el)
+                count++;
+            else
+                count--;
         }
         int count1 = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == el) count1++;
+            if (nums[i] == el)
+                count1++;
         }
-        if (count1 > n / 2) return el;
-        
+        if (count1 > n / 2)
+            return el;
+
         return -1;
+    }
+
+    public static List<Integer> majorityElement3(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > n / 3) {
+                result.add(entry.getKey());
+            }
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
@@ -62,6 +87,10 @@ public class MajorityElement {
         System.out.println(majorityElement(arr));
         System.out.println(majorityElementCount(arr));
         System.out.println(majorityElement2(arr));
+
+        int[] nums1 = { 3, 2, 3 };
+        System.out.println("Input: " + Arrays.toString(nums1));
+        System.out.println("Output: " + majorityElement3(nums1));
 
     }
 }
