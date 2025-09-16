be6d6e32b037cf084482a9dcb6eba80a4c7f2fed
Pratham Potdar <prathampotdar797@gmail.com>
2025-09-16 23:21:27 +0530

    new changes in program

--- Bit/DecimaltoBinary.java
+++ Bit/DecimaltoBinary.java
@@ -1,20 +1,22 @@
 package Bit;
+
 import java.util.*;
+
 public class DecimaltoBinary {
-    public static void DecBin(int n){
-        int myNum  = n;
-        int pow =0;
-        int Bin =0;
-        while(n > 0){
+    public static void DecBin(int n) {
+        int myNum = n;
+        int pow = 0;
+        int Bin = 0;
+        while (n > 0) {
             int rem = n % 2;
-            Bin = Bin + (rem * (int)Math.pow(10,pow));
+            Bin = Bin + (rem * (int) Math.pow(10, pow));
             pow++;
-            n = n/2;
+            n = n / 2;
         }
-        System.out.println("Decimal to "+ myNum +" is :"+ Bin);
+        System.out.println("Decimal to " + myNum + " is :" + Bin);
     }
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Decimal number :");
         int n = sc.nextInt();
--- SpecialSeries/PrimeRange.java
+++ SpecialSeries/PrimeRange.java
@@ -1,26 +1,27 @@
 public class PrimeRange {
-    public static boolean isPrime(int n){
-        if(n == 2){
+    public static boolean isPrime(int n) {
+        if (n == 2) {
             return true;
         } else {
-        for(int i=2;i<=Math.sqrt(n);i++){
-            if(n%i == 0){
-                return false;
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    return false;
+                }
             }
+            return true;
         }
-        return true;
     }
-    }
-    public static void PrimesRange(int n){
-        for(int i=2;i<=n;i++){
-            if(isPrime(i)){
-                System.out.print(i+" ");
+
+    public static void PrimesRange(int n) {
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
             }
         }
         System.out.println();
     }
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         PrimesRange(30);
     }
 }
