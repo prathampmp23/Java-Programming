5baa17ac4725e1cc1b601c86122ea7076ee46f01
Pratham Potdar <prathampotdar797@gmail.com>
2025-09-19 23:30:05 +0530

    new binary search programs added

--- Arrays1D/UpperAndLowerBound.java
+++ Arrays1D/UpperAndLowerBound.java
@@ -1,11 +1,13 @@
 package Arrays1D;
 
-public class FloorAndCeil {
-    public static int[] getFloorAndCeil(int[] arr, int x) {
-        // Write your code here.
-        int floor = getFloor(arr, x);
-        int ceil = getCeil(arr, x);
-        return new int[] { floor, ceil };
+import java.util.Arrays;
+
+public class UpperAndLowerBound {
+    public static int[] getUpperAndLower(int[] arr, int x) {
+        Arrays.sort(arr);
+        int upperBound = getCeil(arr, x);
+        int lowerBound = getFloor(arr, x);
+        return new int[] { upperBound, lowerBound };
     }
 
     static int getFloor(int[] arr, int x) {
@@ -38,11 +40,11 @@ public class FloorAndCeil {
     }
 
     public static void main(String[] args) {
-        int arr[] = { 3, 4, 7, 8, 8, 10 };
-        int x = 5;
-        int ans[] = getFloorAndCeil(arr, x);
-        System.out.println("Floor = " + ans[0]);
-        System.out.println("Ceil = " + ans[1]);
+        int arr[] = { 5, 6, 8, 9, 6, 5, 5, 6 };
+        int x = 7;
+        int ans[] = getUpperAndLower(arr, x);
+        System.out.println("Upper Bound = " + ans[0]);
+        System.out.println("Lower Bound = " + ans[1]);
         System.out.println(getFloor(arr, x));
         System.out.println(getCeil(arr, x));
     }
